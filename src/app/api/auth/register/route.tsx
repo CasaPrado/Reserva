96ac781251f3b8b37326f72e61683a344a46bc94
jonathan alexandre <jonathanagsilva@gmail.com>
2024-10-20 19:28:57 -0400

    20.10.05

--- src/app/api/auth/register/route.tsx
+++ src/app/api/auth/register/route.tsx
@@ -4,38 +4,50 @@ import { prisma } from '@/lib/prisma';
 
 export async function POST(req: Request) {
   try {
-    const { name, email, password } = await req.json();
+    console.time('Tempo total');
 
-    console.log('Recebendo dados para registro:', { name, email });
+    // Registrar o início da leitura do corpo da requisição
+    console.time('Leitura do corpo da requisição');
+    const { name, email, password } = await req.json();
+    console.timeEnd('Leitura do corpo da requisição');
+
+    // Verificar se o usuário já existe
+    console.time('Verificação de usuário existente');
     const existingUser = await prisma.user.findUnique({
       where: { email },
     });
-    console.log("passou!")
+    console.timeEnd('Verificação de usuário existente');
+
     if (existingUser) {
       console.log('Usuário já existe:', email);
+      console.timeEnd('Tempo total');
       return NextResponse.json({ message: 'Usuário já existe' }, { status: 400 });
     }
-    console.log("passou!")
-    // Hash da senha com menos rounds para acelerar o processo
-    const hashedPassword = await bcrypt.hash(password, 4); 
-    console.log("passou!")
+
+    // Hash da senha
+    console.time('Hash da senha');
+    const hashedPassword = await bcrypt.hash(password, 8); // Menos rounds para teste
+    console.timeEnd('Hash da senha');
 
     // Criar novo usuário
+    console.time('Criação do usuário');
     const user = await prisma.user.create({
       data: {
         name,
         email,
         password: hashedPassword,
-        accessLevel: 'user', // Por padrão, novos usuários são 'user'
+        accessLevel: 'user',
       },
     });
+    console.timeEnd('Criação do usuário');
 
     console.log('Usuário criado com sucesso:', user);
-    console.log("passou!")
+    console.timeEnd('Tempo total');
 
     return NextResponse.json({ message: 'Usuário criado com sucesso' }, { status: 201 });
   } catch (error) {
     console.error('Erro ao registrar usuário:', error);
+    console.timeEnd('Tempo total');
     return NextResponse.json({ message: 'Erro interno do servidor' }, { status: 500 });
   }
 }
