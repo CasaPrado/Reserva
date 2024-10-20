259286e1c5cdf34b3a80bdafe3b61b3261b00cef
jonathan alexandre <jonathanagsilva@gmail.com>
2024-10-20 16:31:52 -0400

    20.10.04

--- src/app/api/auth/register/route.tsx
+++ src/app/api/auth/register/route.tsx
@@ -6,17 +6,22 @@ export async function POST(req: Request) {
   try {
     const { name, email, password } = await req.json();
 
+    // Log para verificar a entrada
+    console.log('Recebendo dados para registro:', { name, email });
+
     // Verificar se o usuário já existe
     const existingUser = await prisma.user.findUnique({
       where: { email },
     });
 
+    // Se o usuário já existir, retornar imediatamente
     if (existingUser) {
+      console.log('Usuário já existe:', email);
       return NextResponse.json({ message: 'Usuário já existe' }, { status: 400 });
     }
 
-    // Hash da senha
-    const hashedPassword = await bcrypt.hash(password, 4);
+    // Hash da senha com menos rounds para acelerar o processo
+    const hashedPassword = await bcrypt.hash(password, 8); // Reduzido para 8 para testes
 
     // Criar novo usuário
     const user = await prisma.user.create({
@@ -27,6 +32,8 @@ export async function POST(req: Request) {
         accessLevel: 'user', // Por padrão, novos usuários são 'user'
       },
     });
+
+    console.log('Usuário criado com sucesso:', user);
 
     return NextResponse.json({ message: 'Usuário criado com sucesso' }, { status: 201 });
   } catch (error) {
